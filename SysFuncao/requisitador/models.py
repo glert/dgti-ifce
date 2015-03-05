fdcc41fc6eb7c0b36efc0f69816e36d17c7b77d1
Elizafã <ejailesb@gmail.com>
2015-03-05 17:44:56 -0300

    teste5

--- SysFuncao/requisitador/models.py
+++ SysFuncao/requisitador/models.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import ser
  
 class Sistema(models.Model):
     nome = models.CharField(max_length=128, unique=True)
