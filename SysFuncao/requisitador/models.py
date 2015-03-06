c6ceae818f7ac969e4e5008e27c61ead1c628618
sabia <sabia@Sabia-PC>
2015-03-06 16:29:26 -0300

    Mudanças no modelo. Criação do modelo para models.Mensagem. Uma requisição pode conter várias mensagens para discutir essa requisição. O super usuário é sabia senha abcd1234

--- SysFuncao/requisitador/models.py
+++ SysFuncao/requisitador/models.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import ser
+from django.contrib.auth.models import User
  
 class Sistema(models.Model):
     nome = models.CharField(max_length=128, unique=True)
@@ -9,7 +9,8 @@ class Sistema(models.Model):
     def __unicode__(self):
         return self.nome
  
-class Requisito(models.Model):  
+
+class Requisicao(models.Model):  
     STATUSES_TYPE = (
         (u'novo', u'Novo'),
         (u'análise de viabilidade', u'Análise de viabilidade'),
@@ -17,13 +18,25 @@ class Requisito(models.Model):
         (u'em implementação', u'Em implementação'),
         (u'rejeitado', u'Rejeitado'),
         (u'finalizado e operacional', u'Finalizado e operacional'),
-    )
-    status_id = models.AutoField(primary_key=True)
-    sistema = models.ForeignKey(Sistema)
+    )    
+    sistema = models.ForeignKey(Sistema)    
     status_tipo = models.CharField(max_length=15, choices=STATUSES_TYPE, verbose_name='Status')
-    status_descricao = models.TextField(verbose_name='Descricao')
-    response = models.ForeignKey(Sistema.Responsavel)
+    criador = models.ForeignKey(User, related_name='criador', null=False, blank=False)
+    
+    
+    interessados = models.ManyToManyField(User, related_name='interessados')
     def __unicode__(self):
-        return self.status_descricao
-      
+        return "(%s) %s por %s" % (self.pk, Sistema.objects.get(pk=self.sistema), User.objects.get(pk =self.criador))
+
+class Mensagem(models.Model):
+    dataHora = models.DateTimeField()
+    conteudo = models.TextField();
+    usuario = models.ForeignKey(User)    
+    requisicao_associada = models.ForeignKey(Requisicao)
+    def __unicode__(self):
+        result = str(self.conteudo)
+        if result.len() >= 20:
+            result = '%s...' % result[:18]            
+        return result 
+          
     
