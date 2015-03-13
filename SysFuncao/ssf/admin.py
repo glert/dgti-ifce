129a24626f48a4402584ced7dd1407dec2f10bcb
nbook <nbook@nbook>
2015-03-12 23:27:25 -0300

    Test1

--- SysFuncao/ssf/admin.py
+++ SysFuncao/ssf/admin.py
@@ -3,4 +3,4 @@ from ssf.models import Sistema, Requisitacao2, Requisicao
 
 admin.site.register(Sistema)
 admin.site.register(Requisitacao2)
-admin.site.register(Requisicao)
+admin.site.register(Requisicao
