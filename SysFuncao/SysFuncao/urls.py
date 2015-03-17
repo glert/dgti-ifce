6ed16224fb943efffff732b15094086f45317ccc
Elizafã <ejailesb@gmail.com>
2015-03-17 14:42:20 -0300

    Adicao de um ( na url

--- SysFuncao/SysFuncao/urls.py
+++ SysFuncao/SysFuncao/urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
     url(r'^accounts/loggedin/$', LogadoView.as_view()),
     #url(r'^accounts/logout/$', 'ssf.views.logout'),
     url(r'^accounts/addrequisitos/$', NovaRequisicaoView.as_view()),
-    url(r'^accounts/dialogo/(?P<requisicao_id>)\d\d*$', RequisicaoView.as_view()),
+    url(r'^accounts/dialogo/(?P<requisicao_id>\d\d*)$', RequisicaoView.as_view()),
     #url(r'^form$', 'ssf.views.form'),
     # acao do botao sair
       
